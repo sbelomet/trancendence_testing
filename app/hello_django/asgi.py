43fe0649209ec7feba4b5d06bab8ef416a5cc47c
sean <sbelomet@student.42lausanne.ch>
2024-11-08 14:51:44 +0100

    uhhhh chat app

--- app/hello_django/asgi.py
+++ app/hello_django/asgi.py
@@ -9,8 +9,24 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_django.settings')
 
-application = get_asgi_application()
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+import chat.routing
+
+application = ProtocolTypeRouter(
+	{
+		"http": django_asgi_app,
+		"websocket": AllowedHostsOriginValidator(
+			AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+		),
+	}
+)
--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -16,16 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from upload.views import image_upload
 
 urlpatterns = [
-	path('polls/', include('polls.urls')),
-	#path("", image_upload, name="upload"),
+	path("chat/", include("chat.urls")),
     path('admin/', admin.site.urls),
 ]
-
-if bool(settings.DEBUG):
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
